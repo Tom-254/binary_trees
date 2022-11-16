d4c4c7dc0b2b0b493fa3c3ac9cc5d9359fcd5858
Tom-254 <211mseeratiego@gmail.com>
2022-11-16 14:21:03 +0000

    0x1D. C - Binary trees

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,1 +1,15 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_balance - finds the balance factor of a binary tree
+ * @tree: the tree to find the balance factor of
+ * Return: the balance factor
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+
+}
