9d118f91f03d3fb1a185b63ba8d5d8c605728b5a
Tom-254 <211mseeratiego@gmail.com>
2022-11-16 13:13:34 +0000

    0x1D. C - Binary trees

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,12 +3,15 @@
 /**
  * binary_tree_height - finds the height of a particular node
  * @tree: the node to find the height of
- * Return: ldepth if it is the largest node length or rdepth if not
+ * Return: height of the binary tree
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t height_l;
 	size_t height_r;
+
+    if (!tree)
+		return (0);
 
 	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
